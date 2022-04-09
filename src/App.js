c7749876c74de965a65282dd692a9649d801fede
Márton Bendegúz Bendicsek <bendicsekb@gmail.com>
2022-04-09 15:45:46 +0200

    Feature: Open in new window on the 3rd click
    (cherry picked from commit 50a6a730f8c3b90ded8e6eb9abaa3964e7ae931e)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { quotes } from "./quotes"
+
+// source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+let clicks = 0
+
+function onWindowClick() {
+  clicks += 1
+  if(clicks % 3 == 0){
+    window.open("gabor", "_blank")
+  }
+}
+
+// async function openQuotes() {
+//   await new Promise(r => setTimeout(r, 1000)).then(() => {window.open("gabor", "_blank")})
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {shuffle(quotes).map(line => (
+        <p>{line}</p>
+      )
+      )}
+
+
+      {
+        window.addEventListener("click", onWindowClick)
+      }
+
     </div>
   );
 }
